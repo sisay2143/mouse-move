f896b1df635abd1b75319eeb4cf861011b6ee5c7
sisay <sisaybekele735@gmail.com>
2023-05-22 21:40:36 +0300

    added

---
+++ script.js
@@ -0,0 +1,17 @@
+ const containerEl = document.querySelector(".container");
+window.addEventListener("mousemove", (event) =>  {
+    
+    
+    containerEl.innerHTML = `
+    <div class="mouse-event">
+            ${event.clientX}
+            <h4>Mouse X prosition(px)</h4>
+        </div>
+        <div class="mouse-event">
+               ${event.clientY}
+                <h4>Mouse Y prosition(px)</h4>
+            </div></>
+            `
+}); 
+
+console.log(5)
